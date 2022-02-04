c39f1ab84beed71565e8eff6bddd20638036bab9
pbr4ne <pbr4ne@protonmail.com>
2022-02-03 22:12:08 -0800

    test: parameterize conversoin tests

--- src/conversion/index.test.js
+++ src/conversion/index.test.js
@@ -2,46 +2,16 @@ const { convert } = require('./index');
 
 describe('conversions', () => {
   describe('feet to metres', () => {
-    test('convert ft to m', () => {
-      const conversion = convert('10ft');
-      expect(conversion.length).toBe(1);
-      expect(conversion[0]).toBe('10 ft = 3.05 m');
-    });
-
-    test('convert feet to m', () => {
-      const conversion = convert('10 feet');
-      expect(conversion.length).toBe(1);
-      expect(conversion[0]).toBe('10 ft = 3.05 m');
-    });
-  
-    test('convert ’ to m', () => {
-      const conversion = convert('10’');
-      expect(conversion.length).toBe(1);
-      expect(conversion[0]).toBe('10 ft = 3.05 m');
-    });
-
-    test('convert \' to m', () => {
-      const conversion = convert('10\'');
+    test.each(['ft', ' ft', 'feet', '\'', '’'])('convert %s to m', (a) => {
+      const conversion = convert(`10${a}`);
       expect(conversion.length).toBe(1);
       expect(conversion[0]).toBe('10 ft = 3.05 m');
     });
   });
   
   describe('metres to feet', () => {
-    test('convert m to ft', () => {
-      const conversion = convert('10m');
-      expect(conversion.length).toBe(1);
-      expect(conversion[0]).toBe('10 m = 32.81 ft');
-    });
-
-    test('convert metre to ft', () => {
-      const conversion = convert('10 metre');
-      expect(conversion.length).toBe(1);
-      expect(conversion[0]).toBe('10 m = 32.81 ft');
-    });
-
-    test('convert meter to ft', () => {
-      const conversion = convert('10 meter');
+    test.each(['m', ' m', 'metre', 'metres', 'meters'])('convert %s to m', (a) => {
+      const conversion = convert(`10${a}`);
       expect(conversion.length).toBe(1);
       expect(conversion[0]).toBe('10 m = 32.81 ft');
     });
