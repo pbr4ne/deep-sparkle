d50b30ed9bba75c17c9f05fa1e0e30639e689129
pbr4ne <pbr4ne@protonmail.com>
2022-02-03 21:59:54 -0800

    feat: add metres to conversion

--- src/conversion/index.js
+++ src/conversion/index.js
@@ -1,13 +1,38 @@
 const convert = require('convert-units');
 
-exports.convert = function(content) {
-  const NUMBER_REGEX = new RegExp(/([+-]?\d+(\.\d+)*)/g);
-  const FT_REGEX = new RegExp(/(^| )([+-]?\d+(\.\d+)*)\s?('|’|ft|feet)($|[ ,.:?!])/g);
+const NUMBER_REGEX = new RegExp(/([+-]?\d+(\.\d+)*)/g);
+const FT_REGEX = new RegExp(/(^| )([+-]?\d+(\.\d+)*)\s?('|’|ft|feet)($|[ ,.:?!])/g);
+const M_REGEX = new RegExp(/(^| )([+-]?\d+(\.\d+)*)\s?(m|metre|metres|meter|meters)($|[ ,.:?!])/g);
 
-  const match = FT_REGEX.exec(content);
-  if (match) {
-    const fromNumber = Number.parseFloat(NUMBER_REGEX.exec(match)[0]);
-    const convertedValue = convert(fromNumber).from('ft').to('m').toFixed(2);
-    return `${fromNumber}' = ${convertedValue} m`;
-  }
+const converters = [{
+  regex: FT_REGEX,
+  fromUnit: 'ft',
+  toUnit: 'm',
+},{
+  regex: M_REGEX,
+  fromUnit: 'm',
+  toUnit: 'ft',
+}];
+
+const resetRegex = (regex) => {
+  regex.lastIndex = 0;
+  NUMBER_REGEX.lastIndex = 0;
+};
+
+exports.convert = function(content) {
+  
+  return converters
+    .filter(converter => {
+      resetRegex(converter.regex);
+      return converter.regex.exec(content);
+    })
+    .map(converter => {
+      resetRegex(converter.regex);
+      const match = converter.regex.exec(content);
+      if (match) {
+        const fromNumber = Number.parseFloat(NUMBER_REGEX.exec(match)[0]);
+        const convertedValue = convert(fromNumber).from(converter.fromUnit).to(converter.toUnit).toFixed(2);
+        return `${fromNumber} ${converter.fromUnit} = ${convertedValue} ${converter.toUnit}`;
+      }
+    });
 };
--- src/conversion/index.test.js
+++ src/conversion/index.test.js
@@ -1,11 +1,60 @@
 const { convert } = require('./index');
 
 describe('conversions', () => {
-  test('convert ft to m', () => {
-    expect(convert('10ft')).toBe('10\' = 3.05 m');
+  describe('feet to metres', () => {
+    test('convert ft to m', () => {
+      const conversion = convert('10ft');
+      expect(conversion.length).toBe(1);
+      expect(conversion[0]).toBe('10 ft = 3.05 m');
+    });
+
+    test('convert feet to m', () => {
+      const conversion = convert('10 feet');
+      expect(conversion.length).toBe(1);
+      expect(conversion[0]).toBe('10 ft = 3.05 m');
+    });
+  
+    test('convert ’ to m', () => {
+      const conversion = convert('10’');
+      expect(conversion.length).toBe(1);
+      expect(conversion[0]).toBe('10 ft = 3.05 m');
+    });
+
+    test('convert \' to m', () => {
+      const conversion = convert('10\'');
+      expect(conversion.length).toBe(1);
+      expect(conversion[0]).toBe('10 ft = 3.05 m');
+    });
   });
   
+  describe('metres to feet', () => {
+    test('convert m to ft', () => {
+      const conversion = convert('10m');
+      expect(conversion.length).toBe(1);
+      expect(conversion[0]).toBe('10 m = 32.81 ft');
+    });
+
+    test('convert metre to ft', () => {
+      const conversion = convert('10 metre');
+      expect(conversion.length).toBe(1);
+      expect(conversion[0]).toBe('10 m = 32.81 ft');
+    });
+
+    test('convert meter to ft', () => {
+      const conversion = convert('10 meter');
+      expect(conversion.length).toBe(1);
+      expect(conversion[0]).toBe('10 m = 32.81 ft');
+    });
+  });
+  
+  test('convert multiple', () => {
+    const conversion = convert('10ft 10m');
+    expect(conversion.length).toBe(2);
+    expect(conversion[0]).toBe('10 ft = 3.05 m');
+    expect(conversion[1]).toBe('10 m = 32.81 ft');
+  });
+
   test('no conversion', () => {
-    expect(convert('hello')).toBe(undefined);
+    expect(convert('hello').length).toBe(0);
   });
 });
