e1e7a89b6a0188a7d2efa1120b619138fd53b5ea
pbr4ne <pbr4ne@protonmail.com>
2022-02-05 22:07:11 -0800

    test: parameterize basically all conversion tests
    I bet there's an even more efficent way of doing this

--- src/modules/convert/index.test.js
+++ src/modules/convert/index.test.js
@@ -2,85 +2,77 @@ const { convert } = require('./index');
 
 //todo test for leading spaces
 describe('conversions', () => {
-  describe('feet to metres', () => {
-    test.each(['ft', 'feet', '\'', '’'])('should convert %s to m', (a) => {
-      const conversion = convert(`10${a}`);
-      expect(conversion.fields.length).toBe(1);
-      expect(conversion.fields[0].label).toBe('Converted to meters');
-      expect(conversion.fields[0].content).toBe('10 ft = 3.05 m');
-    });
-  });
+  test.each`
+    inputUnit       | expectedAmount  | expectedInputUnit | expectedOutputUnit  |  expectedOutputUnitLong
+    ${'ft'}         | ${3.05}         | ${'ft'}           | ${'m'}              |  ${'meters'}
+    ${'feet'}       | ${3.05}         | ${'ft'}           | ${'m'}              |  ${'meters'}
+    ${'\''}         | ${3.05}         | ${'ft'}           | ${'m'}              |  ${'meters'}
+    ${'’'}          | ${3.05}         | ${'ft'}           | ${'m'}              |  ${'meters'}
+    ${'m'}          | ${32.81}        | ${'m'}            | ${'ft'}             |  ${'feet'}
+    ${'metre'}      | ${32.81}        | ${'m'}            | ${'ft'}             |  ${'feet'}
+    ${'meter'}      | ${32.81}        | ${'m'}            | ${'ft'}             |  ${'feet'}
+    ${'metres'}     | ${32.81}        | ${'m'}            | ${'ft'}             |  ${'feet'}
+    ${'meters'}     | ${32.81}        | ${'m'}            | ${'ft'}             |  ${'feet'}
+    ${'kg'}         | ${22.05}        | ${'kg'}           | ${'lb'}             |  ${'pounds'}
+    ${'kgs'}        | ${22.05}        | ${'kg'}           | ${'lb'}             |  ${'pounds'}
+    ${'kilo'}       | ${22.05}        | ${'kg'}           | ${'lb'}             |  ${'pounds'}
+    ${'kilos'}      | ${22.05}        | ${'kg'}           | ${'lb'}             |  ${'pounds'}
+    ${'kilograms'}  | ${22.05}        | ${'kg'}           | ${'lb'}             |  ${'pounds'}
+    ${'lb'}         | ${4.54}         | ${'lb'}           | ${'kg'}             |  ${'kilograms'}
+    ${'lbs'}        | ${4.54}         | ${'lb'}           | ${'kg'}             |  ${'kilograms'}
+    ${'pound'}      | ${4.54}         | ${'lb'}           | ${'kg'}             |  ${'kilograms'}
+    ${'pounds'}     | ${4.54}         | ${'lb'}           | ${'kg'}             |  ${'kilograms'}
+    ${'km'}         | ${6.21}         | ${'km'}           | ${'mi'}             |  ${'miles'}
+    ${'kms'}        | ${6.21}         | ${'km'}           | ${'mi'}             |  ${'miles'}
+    ${'kilometre'}  | ${6.21}         | ${'km'}           | ${'mi'}             |  ${'miles'}
+    ${'kilometres'} | ${6.21}         | ${'km'}           | ${'mi'}             |  ${'miles'}
+    ${'kilometer'}  | ${6.21}         | ${'km'}           | ${'mi'}             |  ${'miles'}
+    ${'kilometers'} | ${6.21}         | ${'km'}           | ${'mi'}             |  ${'miles'}
+    ${'mi'}         | ${16.09}        | ${'mi'}           | ${'km'}             |  ${'kilometers'}
+    ${'mis'}        | ${16.09}        | ${'mi'}           | ${'km'}             |  ${'kilometers'}
+    ${'mile'}       | ${16.09}        | ${'mi'}           | ${'km'}             |  ${'kilometers'}
+    ${'miles'}      | ${16.09}        | ${'mi'}           | ${'km'}             |  ${'kilometers'}
+    ${'c'}          | ${50}           | ${'C'}            | ${'F'}              |  ${'degrees fahrenheit'}
+    ${'C'}          | ${50}           | ${'C'}            | ${'F'}              |  ${'degrees fahrenheit'}
+    ${'°c'}         | ${50}           | ${'C'}            | ${'F'}              |  ${'degrees fahrenheit'}
+    ${'°C'}         | ${50}           | ${'C'}            | ${'F'}              |  ${'degrees fahrenheit'}
+    ${'° c'}        | ${50}           | ${'C'}            | ${'F'}              |  ${'degrees fahrenheit'}
+    ${'° C'}        | ${50}           | ${'C'}            | ${'F'}              |  ${'degrees fahrenheit'}
+    ${'deg c'}      | ${50}           | ${'C'}            | ${'F'}              |  ${'degrees fahrenheit'}
+    ${'deg C'}      | ${50}           | ${'C'}            | ${'F'}              |  ${'degrees fahrenheit'}
+    ${'degrees c'}  | ${50}           | ${'C'}            | ${'F'}              |  ${'degrees fahrenheit'}
+    ${'degrees C'}  | ${50}           | ${'C'}            | ${'F'}              |  ${'degrees fahrenheit'}
+    ${'f'}          | ${-12}          | ${'F'}            | ${'C'}              |  ${'degrees celsius'}
+    ${'F'}          | ${-12}          | ${'F'}            | ${'C'}              |  ${'degrees celsius'}
+    ${'°f'}         | ${-12}          | ${'F'}            | ${'C'}              |  ${'degrees celsius'}
+    ${'°F'}         | ${-12}          | ${'F'}            | ${'C'}              |  ${'degrees celsius'}
+    ${'° f'}        | ${-12}          | ${'F'}            | ${'C'}              |  ${'degrees celsius'}
+    ${'° F'}        | ${-12}          | ${'F'}            | ${'C'}              |  ${'degrees celsius'}
+    ${'deg f'}      | ${-12}          | ${'F'}            | ${'C'}              |  ${'degrees celsius'}
+    ${'deg F'}      | ${-12}          | ${'F'}            | ${'C'}              |  ${'degrees celsius'}
+    ${'degrees f'}  | ${-12}          | ${'F'}            | ${'C'}              |  ${'degrees celsius'}
+    ${'degrees F'}  | ${-12}          | ${'F'}            | ${'C'}              |  ${'degrees celsius'}
+  `('should convert $inputUnit to $expectedUnits', ({inputUnit, expectedAmount, expectedInputUnit, expectedOutputUnit, expectedOutputUnitLong}) => {
+    const conversion = convert(`10${inputUnit}`);
+    expect(conversion.fields.length).toBe(1);
+    expect(conversion.fields[0].label).toBe(`Converted to ${expectedOutputUnitLong}`);
+    expect(conversion.fields[0].content).toBe(`10 ${expectedInputUnit} = ${expectedAmount} ${expectedOutputUnit}`);
 
-  describe('metres to feet', () => {
-    test.each(['m', 'metre', 'metres', 'meters'])('should convert %s to ft', (a) => {
-      const conversion = convert(`10${a}`);
-      expect(conversion.fields.length).toBe(1);
-      expect(conversion.fields[0].label).toBe('Converted to feet');
-      expect(conversion.fields[0].content).toBe('10 m = 32.81 ft');
-    });
-  });
-
-  describe('kilograms to pounds', () => {
-    test.each(['kg', 'kgs', 'kilo', 'kilos', 'kilograms'])('should convert %s to lb', (a) => {
-      const conversion = convert(`10${a}`);
-      expect(conversion.fields.length).toBe(1);
-      expect(conversion.fields[0].label).toBe('Converted to pounds');
-      expect(conversion.fields[0].content).toBe('10 kg = 22.05 lb');
-    });
-  });
-
-  describe('pounds to kilograms', () => {
-    test.each(['lb', 'lbs', 'pound', 'pounds'])('should convert %s to kg', (a) => {
-      const conversion = convert(`10${a}`);
-      expect(conversion.fields.length).toBe(1);
-      expect(conversion.fields[0].label).toBe('Converted to kilograms');
-      expect(conversion.fields[0].content).toBe('10 lb = 4.54 kg');
-    });
-  });
-
-  describe('kilometres to miles', () => {
-    test.each(['km', 'kms', 'kilometre', 'kilometres', 'kilometer', 'kilometers'])('should convert %s to mi', (a) => {
-      const conversion = convert(`10${a}`);
-      expect(conversion.fields.length).toBe(1);
-      expect(conversion.fields[0].label).toBe('Converted to miles');
-      expect(conversion.fields[0].content).toBe('10 km = 6.21 mi');
-    });
-  });
-
-  describe('miles to kilometres', () => {
-    test.each(['mi', 'mis', 'mile', 'miles'])('should convert %s to km', (a) => {
-      const conversion = convert(`10${a}`);
-      expect(conversion.fields.length).toBe(1);
-      expect(conversion.fields[0].label).toBe('Converted to kilometers');
-      expect(conversion.fields[0].content).toBe('10 mi = 16.09 km');
-    });
-  });
-
-  describe('C to F', () => {
-    test.each(['c', 'C', '°c', '°C', '° c', '° C', 'deg c', 'deg C', 'degrees c', 'degrees C'])('should convert %s to F', (a) => {
-      const conversion = convert(`10${a}`);
-      expect(conversion.fields.length).toBe(1);
-      expect(conversion.fields[0].label).toBe('Converted to degrees fahrenheit');
-      expect(conversion.fields[0].content).toBe('10 C = 50 F');
-    });
-  });
-
-  describe('F to C', () => {
-    test.each(['f', 'F', '°f', '°F', '° f', '° F', 'deg f', 'deg F', 'degrees f', 'degrees F'])('should convert %s to C', (a) => {
-      const conversion = convert(`10${a}`);
-      expect(conversion.fields.length).toBe(1);
-      expect(conversion.fields[0].label).toBe('Converted to degrees celsius');
-      expect(conversion.fields[0].content).toBe('10 F = -12 C');
-    });
+    const conversionLeadingSpaces = convert(`10 ${inputUnit}`);
+    expect(conversionLeadingSpaces.fields.length).toBe(1);
+    expect(conversionLeadingSpaces.fields[0].label).toBe(`Converted to ${expectedOutputUnitLong}`);
+    expect(conversionLeadingSpaces.fields[0].content).toBe(`10 ${expectedInputUnit} = ${expectedAmount} ${expectedOutputUnit}`);
   });
 
   test('should convert multiple', () => {
-    const conversion = convert('10ft 10m');
-    expect(conversion.fields.length).toBe(2);
+    const conversion = convert('10ft 10m 10c');
+    expect(conversion.fields.length).toBe(3);
     expect(conversion.fields[0].label).toBe('Converted to meters');
     expect(conversion.fields[0].content).toBe('10 ft = 3.05 m');
     expect(conversion.fields[1].label).toBe('Converted to feet');
     expect(conversion.fields[1].content).toBe('10 m = 32.81 ft');
+    expect(conversion.fields[2].label).toBe('Converted to degrees fahrenheit');
+    expect(conversion.fields[2].content).toBe('10 C = 50 F');
   });
 
   test('should not convert if nothing to convert', () => {
