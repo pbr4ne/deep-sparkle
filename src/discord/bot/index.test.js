89971e9c8826a6d5345d4272883d50fcc2810d7d
pbr4ne <pbr4ne@protonmail.com>
2022-02-15 17:23:06 -0800

    test: remove console.log

--- src/discord/bot/index.test.js
+++ src/discord/bot/index.test.js
@@ -2,7 +2,6 @@ const { bot } = require('.');
 
 describe('bot', () => {
   test('should create bot with correct intent bitfield', () => {
-    console.log(bot);
     expect(bot).toMatchObject({
       options: {
         //GUILDS & GUILD_MESSAGES
