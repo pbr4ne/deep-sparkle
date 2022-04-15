9e2251f60e492e873ab3a99eb3f7391b880a01d9
pbr4ne <pbr4ne@protonmail.com>
2022-04-14 19:18:41 -0700

    feat: add clap emoji at the END too

--- src/modules/clap/index.js
+++ src/modules/clap/index.js
@@ -1,18 +1,19 @@
 const logger = require('../../utilities/log')('clap');
 
+const clapEmoji = '👏';
+
 exports.clap = (content) => {
   return new Promise((resolve) => {
     const clapWords = content.split(' ');
-    let clapResponse;
+    let clapResponse = '';
     if (clapWords.length === 1) {
       if (clapWords[0]) {
-        clapResponse = clapWords[0].split('').join(' 👏 ');
-      } else {
-        clapResponse = '👏';
+        clapResponse = clapWords[0].split('').join(` ${clapEmoji} `);
       }
-    } else {
-      clapResponse = clapWords.join(' 👏 ');
+    } else if (clapWords.length > 1) {
+      clapResponse = clapWords.join(` ${clapEmoji} `);
     }
+    clapResponse += ` ${clapEmoji}`;
     logger.info(clapResponse);
     resolve(clapResponse);
   });
--- src/modules/clap/index.test.js
+++ src/modules/clap/index.test.js
@@ -3,16 +3,16 @@ const { clap } = require('.');
 describe('clap', () => {
   test('should split sentence with clap emoji', async () => {
     const clapResponse = await clap('one two three');
-    expect(clapResponse).toBe('one 👏 two 👏 three');
+    expect(clapResponse).toBe('one 👏 two 👏 three 👏');
   });
 
   test('should split word clap emoji', async () => {
     const clapResponse = await clap('one');
-    expect(clapResponse).toBe('o 👏 n 👏 e');
+    expect(clapResponse).toBe('o 👏 n 👏 e 👏');
   });
 
   test('should return one clap if no words', async () => {
     const clapResponse = await clap('');
-    expect(clapResponse).toBe('👏');
+    expect(clapResponse).toBe(' 👏');
   });
 });
