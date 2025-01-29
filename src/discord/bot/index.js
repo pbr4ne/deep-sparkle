8e160ab0a72d3265479a3f9147ecd42c2cb07b41
pbr4ne <pbr4ne@protonmail.com>
2025-01-28 21:55:26 -0800

    feat: new required intents

--- src/discord/bot/index.js
+++ src/discord/bot/index.js
@@ -3,8 +3,10 @@ const { Client, Intents, Partials } = require('discord.js');
 const intents = [
   Intents.FLAGS.DIRECT_MESSAGES,
   Intents.FLAGS.GUILDS,
+  Intents.FLAGS.GUILD_MEMBERS,
   Intents.FLAGS.GUILD_MESSAGES,
   Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
+  Intents.FLAGS.GUILD_PRESENCES,
 ];
 
 const partials = [
